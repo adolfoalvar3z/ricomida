932ae9f1f8cd3f6bcb8d79e7cccf24d42e362991
Adolfo Alvarez <adolfo.alvar3z.p@gmail.com>
2023-04-20 19:55:22 -0400

    final
    para entrega

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,20 +4,6 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 });
 
-//alert
-var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-var alertTrigger = document.getElementById('enviarCorreo')
 
-function alert(message, type) {
-    var wrapper = document.createElement('div')
-    wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
-
-    alertPlaceholder.append(wrapper)
-}
-
-if (alertTrigger) {
-    alertTrigger.addEventListener('click', function () {
-        alert('El correo fue enviado correctamente...', 'success')
-    })
-}
+//carousel
 
